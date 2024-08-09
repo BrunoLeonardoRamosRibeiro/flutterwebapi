6866d530cd279851cad07e9e5d4de3c2b4c4b04b
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2024-08-09 07:19:24 -0300

    Create movie model

---
+++ lib/model/movie_model.dart
@@ -0,0 +1,36 @@
+class Filme {
+  final int id;
+  final String titulo;
+  final String descricao;
+  final int ano;
+  final String foto;
+
+  const Filme({
+    required this.id,
+    required this.titulo,
+    required this.descricao,
+    required this.ano,
+    required this.foto,
+  });
+
+  factory Filme.fromJson(Map<String, dynamic> json) {
+    return Filme(
+      id: int.parse(json["id"]),
+      titulo: json["titulo"],
+      descricao: json["descricao"],
+      ano: int.parse(json["ano"]),
+      foto: json["foto"],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "titulo": titulo,
+      "descricao": descricao,
+      "ano": ano,
+      "foto": foto,
+    };
+  }
+//
+}
